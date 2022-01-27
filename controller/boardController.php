61622503a73fa1e7fdca741360082e491216c03f
SaPH17 <59709896+SaPH17@users.noreply.github.com>
2022-01-27 14:30:48 +0700

    added pin deletion validation

--- controller/boardController.php
+++ controller/boardController.php
@@ -42,6 +42,44 @@
     else if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['action']) && $_POST['action'] == 'delete'){
         $id = $_POST['id'];
 
+        $sql = "SELECT * FROM board_details WHERE board_id = ?";
+        $statement = $conn->prepare($sql);
+        $statement->bind_param("s", $id);
+        $statement->execute();
+        $details = $statement->get_result();
+
+        while($row = $details->fetch_assoc()){
+            $sql = "SELECT * FROM board_details WHERE pin_id = ?";
+            $statement = $conn->prepare($sql);
+            $statement->bind_param("s", $row['pin_id']);
+            $statement->execute();
+            $result = $statement->get_result();
+
+            if($result->num_rows == 1){
+                print_r("asdasd");
+                $sql = "SELECT * FROM pins WHERE id = ?";
+                $statement = $conn->prepare($sql);
+                $statement->bind_param("s", $row['pin_id']);
+                $statement->execute();
+                $data = $statement->get_result()->fetch_assoc();
+
+                $sql = "DELETE FROM board_details WHERE board_id = ? AND pin_id = ?";
+                $statement = $conn->prepare($sql);
+                $statement->bind_param("ss", $row['board_id'], $row['pin_id']);
+                $statement->execute();   
+
+                $sql = "DELETE FROM pins WHERE id = ?";
+                $statement = $conn->prepare($sql);
+                $statement->bind_param("s", $row['pin_id']);
+                $statement->execute();            
+
+                $save_folder = $data['type'] == "video" ? "asset/video" : "asset/image/pin";
+                $file_path = $_SERVER['DOCUMENT_ROOT'] ."/" . $save_folder . "/" . $data['media'];
+
+                unlink($file_path);
+            }
+        }
+
         $sql = "DELETE FROM boards WHERE id = ?";
         $statement = $conn->prepare($sql);
         $statement->bind_param("s", $id);
